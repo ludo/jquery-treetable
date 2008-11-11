4a7dac203fb5d4c7e2f1e7b3240bbf5838d2e8a8
Ludo van den Boom <ludo.vandenboom@cubicphuse.nl>
2008-11-11 11:50:06 +0100

    Add collapse/expand behavior to treeTable plugin.

--- treeTable/src/javascripts/jquery.treeTable.js
+++ treeTable/src/javascripts/jquery.treeTable.js
@@ -14,9 +14,46 @@
 	
 	$.fn.treeTable.defaults = {
 		childPrefix: "child-of-",
+		expandable: true,
 		indent: 19,
-		treeColumn: 0
+		initialState: "collapsed",
+		treeColumn: 0,
+		userSelect: true
 	};
+	
+	// Recursively hide all node's children in a tree
+	$.fn.collapse = function() {
+		childrenOf($(this)).each(function() {
+			$(this).collapse().hide();
+		});
+		
+		return this; // Satisfy jQuery function chain
+	};
+	
+	// Recursively show all node's children in a tree
+	$.fn.expand = function() {
+		childrenOf($(this)).each(function() {
+			if($(this).is(".expanded.parent")) {
+				$(this).expand();
+			}
+			$(this).show();
+		});
+		
+		return this; // Satisfy jQuery function chain
+	};
+	
+	// Toggle an entire branch
+	$.fn.toggleBranch = function() {
+		if($(this).is(".collapsed")) {
+			$(this).removeClass("collapsed").addClass("expanded").expand();
+		}	else {
+			$(this).removeClass("expanded").addClass("collapsed").collapse();
+		}
+
+		return this; // Satisfy jQuery function chain
+	};
+	
+	// === Private functions
 	
 	// Get children of a node
 	function childrenOf(node) {
@@ -36,6 +73,23 @@
 			childrenOf(node).each(function() {
 				$($(this).children("td")[options.treeColumn]).css("padding-left", padding + "px");
 			});
+			
+			if(options.expandable) {
+				// Add expand/collapse button
+				cell.prepend('<span style="margin-left: -' + options.indent + 'px; padding-left: ' + options.indent + 'px" class="expander"></span>');
+				$(cell[0].firstChild).click(function() { node.toggleBranch(); });
+				
+				// Check for a class set explicitly by the user, otherwise set the default class
+				if(!(node.is(".expanded") || node.is(".collapsed"))) {
+				  node.addClass(options.initialState);
+				}
+
+				if(node.is(".collapsed")) {
+					node.collapse();
+				} else if (node.is(".expanded")) {
+					node.expand();
+				}
+			}
 		}
 	};
 })(jQuery);
