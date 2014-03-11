fbc373c4c7b12fd59a594c1bc3b8b1b4efe5542c
Jonathan Senecal <xxinfinityxx@gmail.com>
2014-03-11 00:10:07 -0400

    Fix for nodes with undefined parent

--- javascripts/src/jquery.treetable.js
+++ javascripts/src/jquery.treetable.js
@@ -292,7 +292,7 @@
             this.nodes.push(node);
             this.tree[node.id] = node;
 
-            if (node.parentId != null) {
+            if (node.parentId != null && this.tree[node.parentId]) {
               this.tree[node.parentId].addChild(node);
             } else {
               this.roots.push(node);
