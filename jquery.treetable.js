e6b6b9035ac7a944937e348aaa260e5c7393836b
marko.zivkovic <marko@bvllfish.de>
2014-11-07 15:07:03 +0100

    adding a method toggleNode

--- jquery.treetable.js
+++ jquery.treetable.js
@@ -506,6 +506,21 @@
 
       return this;
     },
+	toggleNode: function(id) {
+      var node = this.data("treetable").tree[id];
+
+      if (node) {
+        if (!node.initialized) {
+          node._initialize();
+        }
+
+        node.toggle();
+      } else {
+        throw new Error("Unknown node '" + id + "'");
+      }
+
+      return this;
+    },
 
     loadBranch: function(node, rows) {
       var settings = this.data("treetable").settings,
